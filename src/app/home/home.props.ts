584b554bd340d55eaed3d06179db07843c719ffd
Krista Sowell <kristalewis18@gmail.com>
2024-07-19 21:33:36 -0400

    added charm and swirled bead cards on home page

--- src/app/home/home.props.ts
+++ src/app/home/home.props.ts
@@ -5,6 +5,7 @@ const jewelryCard: ProductCard = {
     productName: "Jewelry",
     imageFileName: "jewelry",
     viewMoreRoute: RoutePath.JEWELRY,
+    hasViewMoreButton: true,
     description: "",
 }
 
@@ -12,6 +13,7 @@ const nonJewelryCard: ProductCard = {
     productName: "Non Jewelry",
     imageFileName: "non-jewelry",
     viewMoreRoute: RoutePath.NONJEWELRY,
+    hasViewMoreButton: true,
     description: "",
 }
 
@@ -19,7 +21,24 @@ const shadowboxesCard: ProductCard = {
     productName: "Shadowboxes",
     imageFileName: "12-by-12-shadowbox",
     viewMoreRoute: RoutePath.SHADOWBOXES,
+    hasViewMoreButton: true,
     description: "",
 }
 
-export const HOME_PAGE_PRODUCT_CARDS: Array<ProductCard> = [jewelryCard, nonJewelryCard, shadowboxesCard];
+const charmsCard: ProductCard = {
+    productName: "Charms",
+    imageFileName: "charms",
+    viewMoreRoute: null,
+    hasViewMoreButton: false,
+    description: "For items that feature a charm, a cross, a heart, or an angel are available. Alternative charms can be used upon request.",
+}
+
+const siwrlBeadsCard: ProductCard = {
+    productName: "Swirled Beads",
+    imageFileName: "swirled-beads",
+    viewMoreRoute: null,
+    hasViewMoreButton: false,
+    description: "Swirled beads have waves of white intermingled with the flowers to emphasize the coloration of the beads. Any bead product can be upgraded to have swirled beads.",
+}
+
+export const HOME_PAGE_PRODUCT_CARDS: Array<ProductCard> = [jewelryCard, nonJewelryCard, shadowboxesCard, charmsCard, siwrlBeadsCard];
